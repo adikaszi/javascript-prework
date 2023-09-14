68876895d3a135cb3e1ececf0a9daf5dac99d7ab
Adrian Kaszuba <adikaszi@protonmail.com>
2023-09-14 11:11:19 +0200

    5.4.4

--- js/script.js
+++ js/script.js
@@ -4,14 +4,25 @@ console.log('Wylosowana liczba to: ' + randomNumber);
 
 let computerMove = 'nieznany ruch';
 
-if(randomNumber == 1){
-    computerMove = 'kamień';
-}
-if(randomNumber == 2){
-    computerMuve = 'nożyce';
-}
-if(randomNumber == 3){
-    computerMove = 'papier';
-}
+if(randomNumber == 1){computerMove = 'kamień';}
+
+if(randomNumber == 2){ omputerMuve = 'nożyce';}
+
+if(randomNumber == 3){ computerMove = 'papier';}
+
 
 printMessage('Mój ruch to: ' + computerMove);
+
+let playerInput = prompt('Wybierz swój ruch!! 1: kamień. 2:nożyce, 3: papier.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){ playerMove = 'kamień';}
+
+if(playerInput == '2'){ playerMove = 'nożyce';}
+
+if(playerInput =='3'){ playerMove ='papier';}
+
+printMessege('Twój ruch to: ' + playerMove);
